bb589e7c4045e917762742036e54c084828e2b97
Gabriel Trindade <gabrielsvtrindade@gmail.com>
2020-05-18 09:55:51 -0300

    Character movement and camera control implemented

---
+++ RatsNest/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValue;
+    public float rotateSpeed;
+    public float maxViewAngle = 45f;
+    public float minViewAngle = -45f;
+
+    public Transform pivot;
+    public bool invertYAxis;
+
+
+    void Start()
+    {
+        if(!useOffsetValue)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.position = target.position;
+        pivot.transform.parent = target;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void LateUpdate(){
+
+        //Get X position of mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;   
+        target.Rotate(0, horizontal, 0);
+
+        //Get Y position of mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        
+        if(invertYAxis)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        } else 
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+        
+        
+        //Limit up/down camera rotation
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        //Move camera based on current rotation of target and original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+
+
+        transform.position = target.position - (rotation * offset);
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+
+        transform.LookAt(target);
+    
+    }
+}
---
+++ RatsNest/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+    private Vector3 moveDirection;
+
+    public CharacterController charController;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (charController.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+
+        // Move the controller
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+}
